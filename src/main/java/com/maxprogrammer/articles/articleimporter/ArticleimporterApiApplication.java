17e5e762f7316538821797a3be0858f3b58f3414
maxProgrammer <maxwneto.poo@gmail.com>
2022-06-19 21:42:38 -0300

    setup basic / first commit

---
+++ src/main/java/com/maxprogrammer/articles/articleimporter/ArticleimporterApiApplication.java
@@ -0,0 +1,13 @@
+package com.maxprogrammer.articles.articleimporter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticleimporterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArticleimporterApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/maxprogrammer/articles/articleimporter/ArticleimporterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maxprogrammer.articles.articleimporter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticleimporterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
